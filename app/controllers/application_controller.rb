3008ca112f8a3149808bdea426e55b41c7950665
David Wen <dstwen@gmail.com>
2017-05-09 18:46:31 -0700

    Specific header / footer for submit competition page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :landing_page?
+  helper_method :landing_page?, :submit_competition_page?
 
   def landing_page?
     params[:controller] == "pages" && params[:action] == "landing"
+  end
+
+  def submit_competition_page?
+    params[:controller] == "competitions" && params[:action] == "new"
   end
 end
