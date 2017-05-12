5bfdf6c277a764a012ccff19e0f48299a8d4ce5e
David Wen <dstwen@gmail.com>
2017-05-11 22:26:17 -0700

    Add image_url to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506063254) do
+ActiveRecord::Schema.define(version: 20170512052535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170506063254) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.integer  "competition_id"
+    t.string   "image_url"
   end
 
   create_table "milestones", force: :cascade do |t|
