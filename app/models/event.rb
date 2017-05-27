f03741320fdfec7fb2aa87ac857b71c833c54293
David Wen <dstwen@gmail.com>
2017-05-27 11:53:57 -0700

    Add published scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -28,6 +28,8 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :prizes, allow_destroy: true, :reject_if => proc { |p| p['description'].blank? }
   accepts_nested_attributes_for :milestones, allow_destroy: true, :reject_if => proc { |p| p['deadline_at'].blank? && p['description'].blank? }
 
+  scope :published, -> { where("published is true") }
+
   private
 
   def team_size_range
