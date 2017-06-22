906615a511a5c141a6e0f65a3fa55045975ac507
David Wen <dstwen@gmail.com>
2017-06-21 17:38:56 -0700

    Change domain to competitions.kellogg.northwestern.edu

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SENDGRID_USERNAME"],
   :password => ENV["SENDGRID_PASSWORD"],
-  :domain => 'casechamp.herokuapp.com',
+  :domain => 'competitions.kellogg.northwestern.edu',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
