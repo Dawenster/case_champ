4a401baf7974e8becbafddc7b6c9cf10418ad9ab
David Wen <dstwen@gmail.com>
2017-06-28 08:16:26 -0700

    Update success notice for adding / removing interest

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -8,7 +8,7 @@ class InterestsController < ApplicationController
     redirect_link = competition_path(interest.try(:event).try(:competition), tab: Competition::TAB_PARTICIPANTS, event_id: interest.event.try(:id))
 
     if interest.save
-      redirect_to redirect_link, notice: "Interest successfully saved"
+      redirect_to redirect_link, notice: "You just marked your interest for this competition"
     else
       redirect_to redirect_link, alert: interest.errors.full_messages.to_sentence
     end
@@ -19,7 +19,7 @@ class InterestsController < ApplicationController
     redirect_link = competition_path(interest.try(:event).try(:competition), tab: Competition::TAB_PARTICIPANTS, event_id: interest.event.try(:id))
 
     if interest.destroy
-      redirect_to redirect_link, notice: "Interest successfully removed"
+      redirect_to redirect_link, notice: "You are no longer interested in this competition"
     else
       redirect_to redirect_link, alert: interest.errors.full_messages.to_sentence
     end
